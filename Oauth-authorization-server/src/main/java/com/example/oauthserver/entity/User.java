5c848fa7bc77ccb40e0430292dd6f0169018877b
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2023-04-21 10:03:54 +0300

    Oauth-authorization-server implemented

---
+++ Oauth-authorization-server/src/main/java/com/example/oauthserver/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.oauthserver.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(length = 60)
+    private String password;
+
+    private String role;
+    private boolean enabled = false;
+
+}
---
+++ Oauth-authorization-server/src/main/java/com/example/oauthserver/service/CustomUserDetailsService.java
@@ -0,0 +1,50 @@
+package com.example.oauthserver.service;
+
+import com.example.oauthserver.entity.User;
+import com.example.oauthserver.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (Objects.isNull(user)) {
+            throw new UsernameNotFoundException("No user found");
+        }
+        return new org.springframework.security.core.userdetails.User(
+            user.getEmail(),
+            user.getPassword(),
+            user.isEnabled(),
+            true,
+            true,
+            true,
+            getAuthorities(List.of(user.getRole()))
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        roles.stream().map(SimpleGrantedAuthority::new).forEach(authorities::add);
+        return authorities;
+    }
+}
